cbffb234739a9a09d466c4e809d4659ed1c9100e
BlackMalaika <103142787+BlackMalaika@users.noreply.github.com>
2022-06-15 21:02:23 +0100

    more changes

--- I4G009654VZW/blog/models.py
+++ I4G009654VZW/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now=True)
 
 # Create your models here.
